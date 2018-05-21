8c4d91ec89c6172c8182bc8754f89b42e7eb9351
felipe <marcelus20felipe@gmail.com>
2018-05-21 19:21:39 +0100

    added the comments and the draft of the Block class, it will still be revamped

---
+++ main.js
@@ -0,0 +1,28 @@
+/**
+ * Project: MANTOVACOIN
+ * Author: Felipe Mantovani
+ * Date: 21/5/2018
+ */
+
+
+/**
+The Block class will have the following attributes: an Index, timestamp, data and pHash.
+
+ The index just represents the id of that block. When more blocks are created, the ID will increment.
+ The timestamp is the date when the block is created and validated.
+ data is the data itself, it could be a transaction, or a contract, I still do not know what this block chain
+ will be about.
+ the pHash stands for Previous Hash. As this is a block chain, the hash of the current block is related to the previous
+  hash, so the hash of the previous block should be passed as parameter
+ */
+public class Block{
+    constructor(index, timestamp, data, pHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.pHash = pHash;
+        this.hash = '';
+
+    }
+}
+
